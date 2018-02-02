2991fdb9d8447300f562dacbd112e886fe92e741
keruthika <30829818+keruthika@users.noreply.github.com>
2018-02-02 14:14:04 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i;
+    printf("the numbers are a and b");
+    scanf("%d",&a &b);
+    a=b;
+    b=i;
+    i=a;
+    printf("after swapping %d %d, a,b");
+    return 0;
+}
