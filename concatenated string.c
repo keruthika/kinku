d98fdf31f66892cee8d64b9ad1378d5f8f1e05f2
keruthika <30829818+keruthika@users.noreply.github.com>
2018-02-06 10:30:15 +0000

    Create concatenated string.c

---
+++ concatenated string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  char a[10];
+  char b[10];
+	printf("enter string 1 \n");
+  scanf("%s",&a);
+	printf("enter string 2 \n");
+	scanf("%s",&b);
+	printf("concatenated strings is %s%s"a,b);
+	return 0;
+}
