39476d69245a5a4c4db9982844b20bcb8604f8f6
keruthika <30829818+keruthika@users.noreply.github.com>
2018-02-06 10:37:30 +0000

    Create range.c

---
+++ range.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+printf("\nEnter the value of i");
+scanf("\n%d",&i);
+printf("\nEnter the value of j");
+scanf("\n%d",&j);
+if(i>=j)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
