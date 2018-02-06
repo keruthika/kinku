4938f59d7e0a6bb012443af5d2ca0324f5642fda
keruthika <30829818+keruthika@users.noreply.github.com>
2018-02-06 11:26:03 +0000

    Create pow2.c

---
+++ pow2.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h> 
+int main()
+{
+    int n;
+    int temp,flag;
+    printf("Enter an integer number");
+    scanf("%d",&n);
+    temp=n;
+    flag=0;
+    while(temp!=1)
+    {
+        if(temp%2!=0)
+        {
+            flag=1;
+            break;
+}
+}
+return 0;
+}
