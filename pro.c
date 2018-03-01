327943d27ac67619a45f2d8b624d7f978c7f7439
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-01 15:55:35 +0530

    Create pro.c

---
+++ pro.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,i;
+printf("\nEnter a number");
+scanf("%d",&n);
+i=sqrt(n);
+if(i==(n*n))
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");
+}
+}
+
+    
