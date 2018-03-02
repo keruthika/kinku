f8ac340b2688f3991cd878d41260e947a866cb91
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-02 14:42:46 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,gcd,i;
+scanf("%d %d",&a,&b);
+for(i=1;i<=b && i<=b;++i)
+{
+  if(a%i==0 && b%i==0)
+  gcd = i;
+}
+printf"(\nGCD is %d",gcd);
+return 0;
+}
