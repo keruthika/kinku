808a22e16625736e3a7a22283f02f66a204c4bbf
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-08 16:35:32 +0530

    Create lbh.c

---
+++ lbh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,h,vol=0;
+    printf("Enter length,breadth,height");
+    scanf("%d%d%d",&l,&b,&h);
+    vol=l*b*h;
+    printf("Volume %d",vol);
+    
+}
+
+   
