8bc51325183c91a845371972550b597d03a2be24
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-13 11:01:01 +0000

    Create print.c

---
+++ print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,n,j,d;
+    printf("enter 3 numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    j=a*b;
+    d=j%c;
+    printf("%d",d);
+    return 0;
+}
+    
