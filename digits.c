1923a4c6379d6e420d92b528f41db9cadc35dc50
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-13 11:07:01 +0000

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,i;
+    printf("enter the digits");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    i=a*b*c*d;
+    printf("%d",i);
+    return 0;
+}
+    
