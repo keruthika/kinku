8b3729b6b92f0a1b64898f2d131ff45f9da0401b
keruthika <30829818+keruthika@users.noreply.github.com>
2018-03-13 10:28:50 +0000

    Rename composite.c to com.c

---
+++ com.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i,n,count=0;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%i==0)
+    {
+        count++;
+            }
+            else if(count==1)
+            {
+                printf("not a composite number");
+            }
+        else
+    {
+        printf("composite number");
+    }
+    return 0;
+}
